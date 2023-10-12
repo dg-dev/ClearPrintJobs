ff1babb042bb0f0a9412abda97374aaa971d74ae
Dennis G. <dennis.gruzdev@gmail.com>
2023-10-12 17:19:31 +0800

    Improve logs, increase log size and adjust timings
    - Add detailed logging of printers and print jobs if enabled
    - Adjust log rotation to 10MB
    - Reduced waiting for service delay to speed up the script

--- ClearPrintJobs.ps1
+++ ClearPrintJobs.ps1
@@ -1,12 +1,13 @@
-# ClearPrintJobs.ps1 v1.2 2023-06-21 by Dennis G.
+# ClearPrintJobs.ps1 v1.3 2023-07-05 by Dennis G.
  
-# Yeets all print jobs when jobs over certain age are found to fix stuck printer queues.
-# To be run by Task Scheduler every ~5 minutes until a better solution is found.
+# Fixes stuck printer queues. Yeets all print jobs when erroneous or old jobs are found.
+# To be run by Task Scheduler on an interval until a better solution is found.
 
 $jobsDir = "C:\Windows\System32\spool\PRINTERS"
 $jobExts = ".spl", ".shd"
 $maxJobAgeMins = 17
-$maxLogSizeBytes = 1024 * 1024 * 1
+$maxLogSizeBytes = 1024 * 1024 * 10
+$logMoreDetails = $True
 
 $scriptName = (Get-Item $PSCommandPath).BaseName
 $scriptDir = (Get-Item $PSCommandPath).Directory
@@ -46,6 +47,15 @@ if ((Test-Path -Path $logPath -PathType Leaf) -and ((Get-Item -Path $logPath).le
 $SpoolerService = Get-Service -ServiceName Spooler
 Write-Log ('  Spooler service status: {0}' -f $SpoolerService.Status)
 
+Write-Log -Value ("  Checking printers")
+$allPrinters = @(Get-Printer | Where-Object -Property Shared -EQ $True)
+$abnormalPrinters = @($allPrinters | Where-Object -Property PrinterStatus -NE "Normal")
+Write-Log -Value ("    Found {0} out of {1} printers with abnormal status" -f $abnormalPrinters.Length, $allPrinters.Length)
+Write-Log -Value ("  Checking print job queue")
+$allPrintJobs = @($allPrinters | Get-PrintJob)
+$abnormalPrintJobs = @($allPrintJobs | Where-Object -Property JobStatus -NE "Normal")
+Write-Log -Value ("    Found {0} out of {1} jobs with abnormal status" -f $abnormalPrintJobs.Length, $allPrintJobs.Length)
+#$allPrintJobs | ForEach-Object -Process { }
 Write-Log -Value ("  Checking {0}" -f $jobsDir)
 $allJobFiles = Get-ChildItem -Path $jobsDir -File -Force |
     Where-Object -FilterScript { $PSItem.Extension -in $jobExts }
@@ -61,13 +71,26 @@ $allJobFiles | ForEach-Object -Process {
         $ageString = ' [{0} mins]' -f [int]$ageMins
         Write-Log -Value ("      {0}{1}{2}" -f $PSItem.Name, $ageString, $ageNote)
     }
+if ($allJobFiles -and $logMoreDetails) {
+    Write-Log -Value ("    Additional information")
+    Write-Log -Value ("      Start")
+    #$additionalInfo = Get-Printer | Where-Object -Property Shared -EQ True | Where-Object -Property JobCount -NE 0
+    $additionalInfo = Get-Printer | Where-Object -Property Shared -EQ $True
+    $additionalInfo | Format-List | Out-File -FilePath $logPath -Encoding utf8 -Append -NoClobber -Force
+    $additionalInfo | Format-List
+    $additionalInfo = $additionalInfo | Get-PrintJob
+    $additionalInfo | Format-List | Out-File -FilePath $logPath -Encoding utf8 -Append -NoClobber -Force
+    $additionalInfo | Format-List
+    Write-Log -Value ("      End")
+}
+#if (!$oldJobFiles -and !$abnormalPrintJobs) { exit 0 }
 if (!$oldJobFiles) { exit 0 }
 
 Write-Log ('  Preparing to delete all printer jobs; {0} files' -f $allJobFiles.Length)
 if (-Not ($SpoolerService.Status -EQ 'Stopped')) {
     Write-Log '    Stopping Spooler service'
     $SpoolerService | Stop-Service -Force
-    $SpoolerService.WaitForStatus("Stopped", '00:00:30')
+    $SpoolerService.WaitForStatus("Stopped", '00:00:15')
     $SpoolerService.Refresh()
     Write-Log ("    Spooler service status: {0}" -f $SpoolerService.Status)
 }
@@ -87,7 +110,7 @@ if ($SpoolerService.Status -EQ "Stopped") {
 }
 Write-Log '    Starting Spooler service'
 $SpoolerService | Start-Service
-$SpoolerService.WaitForStatus("Running", '00:00:30')
+$SpoolerService.WaitForStatus("Running", '00:00:15')
 $SpoolerService.Refresh()
 Write-Log ('    Spooler service status: {0}' -f $SpoolerService.Status)
 
